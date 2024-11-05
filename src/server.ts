3eda6da158cefd7a61d016d8f70b6b5437425a56
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2024-11-05 14:40:02 -0600

    chore: move config

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import App from './app';
 import PostsController from './posts/posts.controller';
 
 import 'dotenv/config';
-import config from '../config';
+import config from './config';
 const {MONGO_URI, PORT} = config;
 
 import mongoose from 'mongoose';
