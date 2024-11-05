37c6a81eb1ec07832b1ebedb276d8e30f1c66695
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2024-11-05 13:27:57 -0600

    feat(env): add env vars

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import App from './app';
 import PostsController from './posts/posts.controller';
+
+import 'dotenv/config';
+import config from '../config';
+const {MONGO_URI, PORT} = config;
+
+import mongoose from 'mongoose';
  
 const app = new App(
   [
     new PostsController(),
   ],
-  5001,
+  PORT || 5000,
 );
+
+mongoose.connect(MONGO_URI);
  
 app.listen();
